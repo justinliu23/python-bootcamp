f79734c8d6791e4f3b9dd270cceea636eab5a541
Justin Liu <justinliu23@gmail.com>
2021-08-02 15:28:10 -0700

    Workshop 1 code

---
+++ workshop1.py
@@ -0,0 +1,38 @@
+print("Hello World!")
+# this is a comment (ctrl + /)
+'''This
+is
+a
+multi-line
+comment'''
+# prints a blank/empty line
+print()
+print('Hello World 2')
+
+my_name = 'justin'
+print(my_name)
+
+a, b = 3, 4
+print(my_name + ' is ' + str(a + b) + ' years old.')
+
+# put constants at top normally - something that never changes
+PERCENTAGE_FACTOR = 100
+
+javaVar = 'java'
+python_var = 'python'
+
+two = '2'
+number = 4
+print(int(two) + number)
+
+print(number ** 0.5)
+# useful for determining if a number is odd or even
+print(number % 2)
+
+lots_of_hellos = 'hello' * 10
+print(lots_of_hellos)
+
+password = input()
+print('Your password is ' + str(password))
+
+# A Good Resource: http://pythontutor.com/visualize.html#mode=edit  allows you to visualize your code in real-time, line by line.
